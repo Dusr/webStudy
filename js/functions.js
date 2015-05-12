205ee91af3f75819d7738b8cacebf421aac78485
Dusr <david.a.u@hotmail.com>
2015-05-12 20:03:33 +0200

    rwer

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-
+﻿
 /* Desplegable FAQ*/
 $(document).ready(function () {
 		
@@ -66,7 +66,6 @@ function validarEmail(e) {
     
     if (!expr.test(e.value)) {
         e.setAttribute('placeholder', 'Ej. de correo: mariaperez@hotmail.com');
-        e.value='';
         e.value = '';
     }
 }
@@ -82,13 +81,12 @@ function validarContrasena() {
         pass.value = '';
         pass.setAttribute('placeholder', 'La contraseña debe tener minimo 6 caracteres.');
     }
-    
-    if (rpass.value.length > 0) {
-        if (rpass.value.length < 6) {
-            rpass.value = '';
-            rpass.setAttribute('placeholder', 'La contraseña debe tener minimo 6 caracteres.');
-        }
-    }              
+
+   if (rpass.value.length < 6) {
+       rpass.value = '';
+       rpass.setAttribute('placeholder', 'La contraseña debe tener minimo 6 caracteres.');
+ }
+}              
 
  
 
